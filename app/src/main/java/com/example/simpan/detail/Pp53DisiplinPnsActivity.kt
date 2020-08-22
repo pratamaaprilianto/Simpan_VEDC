87dcd305124370e1b948d97fe651e2ade531d93a
eko630 <58101952+eko630@users.noreply.github.com>
2020-08-22 11:13:47 +0700

    revisi 4

--- app/src/main/java/com/example/simpan/detail/Pp53DisiplinPnsActivity.kt
+++ app/src/main/java/com/example/simpan/detail/Pp53DisiplinPnsActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.*
 import com.example.simpan.MainActivity
 import com.example.simpan.R
 import kotlinx.android.synthetic.main.activity_pp53_disiplin_pns.*
@@ -24,7 +24,66 @@ class Pp53DisiplinPnsActivity : AppCompatActivity() {
         button_kembali.setOnClickListener{
             onBackPressed()
         }
-      }
+        var tahun_selected : String
+        var bulan_selected : String
+
+        tahun_selected = ""
+        bulan_selected = ""
+
+        val text_jml_terlambat = findViewById<TextView>(R.id.tv_jml_terlambat)
+        val text_jml_psw = findViewById<TextView>(R.id.tv_jml_psw)
+        val text_total = findViewById<TextView>(R.id.tv_total)
+
+        val spinner_tahun : Spinner = findViewById(R.id.spin_tahunpp53)
+        val spinner_bulan : Spinner = findViewById(R.id.spin_bulanpp53)
+
+        val array_tahun = arrayOf("2018","2019","2020");
+        val array_bulan = arrayOf("Januari","Februari","Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember")
+
+        spinner_tahun.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,array_tahun)
+        spinner_bulan.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,array_bulan)
+
+        spinner_tahun.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+            }
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                tahun_selected = array_tahun.get(p2);
+            }
+        }
+        spinner_bulan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                bulan_selected = array_bulan.get(p2);
+
+                if (tahun_selected.equals("2018") && bulan_selected.equals("Januari")) {
+                    text_jml_terlambat.text = "00:28:29"
+                    text_jml_psw.text = "0"
+                    text_total.text = "40"
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("Februari")) {
+                    text_jml_terlambat.text = "00:10:00"
+                    text_jml_psw.text = "0"
+                    text_total.text = "20"
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("Maret")) {
+                    text_jml_terlambat.text = "00:00:00"
+                    text_jml_psw.text = "0"
+                    text_total.text = "10"
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("April")) {
+                    text_jml_terlambat.text = "00:15:00"
+                    text_jml_psw.text = "2"
+                    text_total.text = "15"
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("Mei")) {
+                    text_jml_terlambat.text = "00:20:50"
+                    text_jml_psw.text = "0"
+                    text_total.text = "20"
+                }
+            }
+        }
+    }
     }
 
 
--- app/src/main/java/com/example/simpan/detail/TunkinActivity.kt
+++ app/src/main/java/com/example/simpan/detail/TunkinActivity.kt
@@ -2,7 +2,8 @@ package com.example.simpan.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.*
 import com.example.simpan.R
 
 class TunkinActivity : AppCompatActivity() {
@@ -15,6 +16,93 @@ class TunkinActivity : AppCompatActivity() {
         button_kembali.setOnClickListener{
             onBackPressed()
         }
+        var tahun_selected : String
+        var bulan_selected : String
 
+        tahun_selected = ""
+        bulan_selected = ""
+
+        val text_akumulasi_kerja = findViewById<TextView>(R.id.tv_akumulasikurangjam)
+        val text_pengurangan_tunkin = findViewById<TextView>(R.id.tv_pengurangntunkin)
+        val text_potongan_kinerja = findViewById<TextView>(R.id.tv_potongankinerja)
+        val text_total_potongan = findViewById<TextView>(R.id.tv_totalpotongan)
+        val text_poin_tunkin = findViewById<TextView>(R.id.tv_pointunkin)
+        val text_hitung_skp = findViewById<TextView>(R.id.tv_hitungskp)
+        val text_nilai_tunkin = findViewById<TextView>(R.id.tv_nilaitunkin)
+
+        val spinner_tahun : Spinner = findViewById(R.id.spin_tahuninfotunjangan)
+        val spinner_bulan : Spinner = findViewById(R.id.spin_bulaninfotunjangan)
+
+        val array_tahun = arrayOf("2018","2019","2020");
+        val array_bulan = arrayOf("Januari","Februari","Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember")
+
+        spinner_tahun.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,array_tahun)
+        spinner_bulan.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,array_bulan)
+
+        spinner_tahun.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+            }
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                tahun_selected = array_tahun.get(p2);
+            }
+        }
+        spinner_bulan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                bulan_selected = array_bulan.get(p2);
+
+                if (tahun_selected.equals("2018") && bulan_selected.equals("Januari")) {
+                    text_akumulasi_kerja.text = "00:00:00"
+                    text_pengurangan_tunkin.text = "0"
+                    text_potongan_kinerja.text = "0,0"
+                    text_total_potongan.text = "0,0"
+                    text_poin_tunkin.text = "100"
+                    text_hitung_skp.text = "0,0"
+                    text_nilai_tunkin.text = "0,0"
+
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("Februari")) {
+                    text_akumulasi_kerja.text = "00:00:00"
+                    text_pengurangan_tunkin.text = "2"
+                    text_potongan_kinerja.text = "0"
+                    text_total_potongan.text = "2"
+                    text_poin_tunkin.text = "80"
+                    text_hitung_skp.text = "0,0"
+                    text_nilai_tunkin.text = "0,0"
+
+                } else if (tahun_selected.equals("2018") && bulan_selected.equals("Maret")) {
+                    text_akumulasi_kerja.text = "00:00:00"
+                    text_pengurangan_tunkin.text = "0"
+                    text_potongan_kinerja.text = "0"
+                    text_total_potongan.text = "50"
+                    text_poin_tunkin.text = "20"
+                    text_hitung_skp.text = "100"
+                    text_nilai_tunkin.text = "80"
+
+                }else if (tahun_selected.equals("2018") && bulan_selected.equals("April")) {
+                    text_akumulasi_kerja.text = "00:15:00"
+                    text_pengurangan_tunkin.text = "0"
+                    text_potongan_kinerja.text = "0"
+                    text_total_potongan.text = "0"
+                    text_poin_tunkin.text = "80"
+                    text_hitung_skp.text = "20"
+                    text_nilai_tunkin.text = "70"
+
+                }else if (tahun_selected.equals("2018") && bulan_selected.equals("Mei")) {
+                    text_akumulasi_kerja.text = "00:20:00"
+                    text_pengurangan_tunkin.text = "1"
+                    text_potongan_kinerja.text = "1"
+                    text_total_potongan.text = "1"
+                    text_poin_tunkin.text = "90"
+                    text_hitung_skp.text = "0,0"
+                    text_nilai_tunkin.text ="50"
+                }
+            }
+
+        }
     }
 }
